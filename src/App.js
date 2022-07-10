eeb77f492eacc393648e0fbb9a58c70129ee9a12
kokoro <cocosyu.11261126@gmail.com>
2022-07-10 15:54:23 +0900

    Fix: モデルを使用する方法に書き換え

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
 import { element, render } from "./view/html-util.js";
+import { TodoListModel } from "./model/TodoListModel.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
 
 export class App {
+  // TodoListModelの初期化
+  #todoListModel = new TodoListModel();
+
   mount() {
-    const inputElement = document.getElementById('js-form-input');
     const formElement = document.getElementById('js-form');
+    const inputElement = document.getElementById('js-form-input');
     const containerElement = document.getElementById('js-todo-list');
     const todoItemCountElement = document.getElementById('js-todo-count');
 
-    // todoリストを囲むlist要素
-    const todoListElement = element`<ul />`;
-
-    // todoアイテムカウント
-    let todoItemCount = 0;
-    formElement.addEventListener('submit', event => {
-      event.preventDefault();
-      // 追加するTodoアイテムの要素(li要素)を作成する
-      const todoItemElement = element`<li>${inputElement.value}</li>`;
-      // TodoアイテムをtodoListElementに追加する
-      todoListElement.appendChild(todoItemElement);
+    //  TodoListModelの状態が更新されたら値を更新する
+    this.#todoListModel.onChange(() => {
+      // TodoリストをまとめるList要素
+      const todoListElement = element`<ul />`;
+      // それぞれのTodoItem要素をtodoListElement以下へ追加する
+      const todoItems = this.#todoListModel.getTodoItems();
+      todoItems.forEach(item => {
+        // 完了ならchecked属性を付与、未完了ならchecked属性を外す
+        const todoItemElement = item.completed
+          ? element`<li><input type="checkbox" class="checkbox" checked>${item.title}</li>`
+          : element`<li><input type="checkbox" class="checkbox">${item.title}</li>`;
+        todoListElement.appendChild(todoItemElement);
+      });
+      const todoItemElement = element`<li><input type="checkbox" class="checkbox">${item.title}</li>`;
+      const inputCheckboxElement = todoItemElement.querySelector('.checkbox');
+      inputCheckboxElement.addEventListener('change', () => {
+        this.#todoListModel.updateTodo({
+          id: item.id,
+          completed: !item.completed
+        })
+      })
       // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
       render(todoListElement, containerElement);
-      // Todoアイテム数を+1し、表示されてるテキストを更新する
-      todoItemCount++;
-      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
-      // 入力欄を空文字列にしてリセットする
-      inputElement.value = "";
-    }); 
+      // アイテム数の表示を更新
+      todoItemCountElement.textContent = `Todoアイテム数: ${this.#todoListModel.getTotalCount()}`
+    });
+
+    // 値が入力されたら、新しいTodoListModelを追加する
+    formElement.addEventListener('submit', event => {
+      event.preventDefault();
+      this.#todoListModel.addTodo(new TodoItemModel({
+        title: inputElement.value,
+        completed: false
+      }));
+      inputElement.value = '';
+    })
   }
 }
